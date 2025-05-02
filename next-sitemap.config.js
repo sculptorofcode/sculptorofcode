e6b646e31a682b5272bd1b18c4d12e7042a339de
Saikat Roy <107996037+sculptorofcode@users.noreply.github.com>
2025-05-02 17:31:15 +0530

    Update

--- next-sitemap.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    siteUrl: 'https://cyber-saikat.vercel.app/',
-    generateRobotsTxt: true,
-};
--- src/context/textTransform.tsx
+++
@@ -1,5 +0,0 @@
-if (!String.prototype.capitalize) {
-    String.prototype.capitalize = function() {
-      return this.charAt(0).toUpperCase() + this.slice(1); 
-    };
-  }
